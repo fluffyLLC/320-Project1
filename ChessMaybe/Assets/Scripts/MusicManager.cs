0a29fe0abeb0c542da8239fc5caad3ca95396743
Dominic Modderman <djmodd@live.com>
2020-10-20 14:08:19 -0400

    MUSIC

--- ChessMaybe/Assets/Scripts/MusicManager.cs
+++ ChessMaybe/Assets/Scripts/MusicManager.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    public AudioClip[] audioClips;
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
+        if (!audioSource.isPlaying) {
+            int nextSong = Random.Range(0, audioClips.Length - 1);
+
+            audioSource.clip = audioClips[nextSong];
+            audioSource.Play();
         
+        }
+
+
     }
 
     // Update is called once per frame
